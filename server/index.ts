9c0e6145f379a51600a674ddb5e4197660ae264d
andy <theninjawoohoo@hotmail.com>
2021-11-04 21:02:45 -0700

    Found some really good boiler code

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,35 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
+import express, { Express, Request, Response } from 'express';
+import * as http from 'http';
+import next, { NextApiHandler } from 'next';
+import * as socketio from 'socket.io';
 
-const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port: number = parseInt(process.env.PORT || '3000', 10);
+const dev: boolean = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const nextHandler: NextApiHandler = nextApp.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+nextApp.prepare().then(async() => {
+    const app: Express = express();
+    const server: http.Server = http.createServer(app);
+    const io: socketio.Server = new socketio.Server();
+    io.attach(server);
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
-})
+    app.get('/hello', async (_: Request, res: Response) => {
+        res.send('Hello World')
+    });
+
+    io.on('connection', (socket: socketio.Socket) => {
+        console.log('connection');
+        socket.emit('status', 'Hello from Socket.io');
+
+        socket.on('disconnect', () => {
+            console.log('client disconnected');
+        })
+    });
+
+    app.all('*', (req: any, res: any) => nextHandler(req, res));
+
+    server.listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`);
+    });
+});
